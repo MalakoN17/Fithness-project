bdeb5c3b818b7b49918580bc0e2b7178c48a5089
MalakoN17 <bennynagato17@gmail.com>
2022-10-12 18:21:40 +0300

    update

--- src/Components/ExerciseVideos.jsx
+++ src/Components/ExerciseVideos.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
-function ExerciseVideos() {
+import {Box, Stack, Typography} from '@mui/material'
+function ExerciseVideos({exerciseVideos, name}) {
   return (
-    <div>ExerciseVideos</div>
+    <Box sx={{marginTop:{lg: '200px', xs:'20px'}}} p='20px'>
+      <Typography variant='h3' mb='33px'>
+        Watch <span style={{color:'#ff2625', textTransform:'capitalize'}}>{name}</span> exercise videos
+      </Typography>
+
+    </Box>
   )
 }
 
